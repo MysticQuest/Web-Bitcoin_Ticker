868646c38df9f3f6f68f392d76ac1ebe8e2a6522
Mystic Quest <l1skou@gmail.com>
2019-10-30 18:44:51 +0200

    added: get and parse data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
@@ -10,6 +11,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function(req, res){
+  request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body) {
+ var data = JSON.parse(body);
+ var price = data.last;
+ console.log(price);
+  });
+});
+
+
 
 app.listen(3000, function(){
   console.log("Server is running on port: 3000");
